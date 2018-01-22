60df96bf0fceaf214464211df6601f043f25952f
Adi Stef <hardtomakeanadress@hotmail.com>
2018-01-22 18:00:51 +0000

    Add method to check what user is logged; changed favicon

--- models/Cookie.js
+++ models/Cookie.js
@@ -6,7 +6,7 @@ class Cookie {
         document.cookie = `loggedUserToken=${token}`;
     }
     
-    static findLoggedUser() {
+    static findLoggedUserToken() {
         const match = document.cookie.match(new RegExp('loggedUserToken=([^;]+)'));
         if (match) return match[1];
     }
@@ -14,5 +14,10 @@ class Cookie {
     static deleteTokenCookie() {
         document.cookie = 'loggedUserToken=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';   
         document.cookie = 'loggedUser=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+    
+    static findLoggeedUserName() {
+    const match = document.cookie.match(new RegExp('loggedUser=([^;]+)'));
+        if (match) return match[1];   
     }
 }
